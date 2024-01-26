f31f3fe917a4100e5f1fa651bce9df139210024b
ftomaz-c <fabiocastim@gmail.com>
2024-01-26 15:24:53 +0000

    added documentation template to add to each function and documented history.c functions

--- src/history.c
+++ src/history.c
@@ -3,15 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   history.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ftomazc <ftomazc@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ftomazc < ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 14:43:46 by ftomazc           #+#    #+#             */
-/*   Updated: 2024/01/26 14:48:44 by ftomazc          ###   ########.fr       */
+/*   Updated: 2024/01/26 15:20:50 by ftomazc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+/**
+ * @brief Counts the number of lines in a file.
+ * 
+ * This function opens the specified file in read-only mode and counts the number of lines
+ * by repeatedly reading lines until the end of the file is reached.
+ * 
+ * @param filename The name of the file to count lines from.
+ * 
+ * @return The number of lines in the file if successful, or -1 if an error occurs.
+ * 
+ * @note This function assumes that the file exists and is accessible.
+ *       It uses the get_next_line function to read lines from the file.
+ *       The memory for each line is allocated dynamically and freed after use.
+ */
+
+// Function to count the number of lines in a file
 int count_lines_in_file(const char *filename)
 {
 	int		fd;
@@ -34,6 +50,31 @@ int count_lines_in_file(const char *filename)
 	return(count);
 }
 
+/**
+ * @brief Adds a line to a history file and updates the line count.
+ * 
+ * This function appends the provided line to the history file ".minishell_history",
+ * along with the current line count followed by a period and space.
+ * It also updates the line count by reading the current number of lines in the history file.
+ * 
+ * @param line The line to be added to the history file. This should be a null-terminated string
+ *             obtained from the readline function or similar. The function does not modify the contents of the string.
+ * 
+ * @note The function assumes that ".minishell_history" exists in the current directory.
+ *       If the file does not exist, it will be created.
+ *       The function also assumes that the provided line is properly formatted and does not contain newline characters,
+ *       as newline characters will be automatically appended to the line when written to the file.
+ *       If the file or line cannot be processed, appropriate error messages will be printed to stderr.
+ *       Ensure that the file has appropriate permissions for writing.
+ *       The function may allocate additional memory temporarily while reading the file.
+ * 
+ * @warning This function does not handle concurrent access to the history file. If multiple instances of the program
+ *           attempt to write to the history file simultaneously, unexpected behavior may occur.
+ * 
+ * @see count_lines_in_file
+ */
+
+// Function to add a line to a history file
 void    add_history_file(char *line)
 {
 	int	fd;
