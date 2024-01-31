f0e692d4e60041ca751f1d5fd4e385c7caaec838
ftomaz-c <fabiocastim@gmail.com>
2024-01-31 15:42:00 +0000

    added struct s_tools and tools.c

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ftomazc <ftomazc@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ftomazc < ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 10:38:16 by ftomazc           #+#    #+#             */
-/*   Updated: 2024/01/26 14:45:59 by ftomazc          ###   ########.fr       */
+/*   Updated: 2024/01/31 12:58:00 by ftomazc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,22 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
+
+typedef struct	s_tools
+{
+	char	**env;
+	char	**path;
+	char	*pwd;
+	char	*oldpwd;
+}	t_tools;
+
+/*tools.c*/
+int		config_tools(t_tools *tools, char **envp);
+char	**get_env(char **envp);
+char	**get_path(char **env);
+char 	*get_var_from_env(char **envp, char *var);
+void	free_tools(t_tools *tools);
+
 
 /*history.c*/
 void    add_history_file(char *line);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ftomazc < ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 10:37:33 by ftomazc           #+#    #+#             */
-/*   Updated: 2024/01/29 16:07:32 by ftomazc          ###   ########.fr       */
+/*   Updated: 2024/01/31 13:16:28 by ftomazc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@
 int main(int argc, char **argv, char **envp)
 {
 	char	*line;
+	t_tools	tools;
 
-	error_check(argc, argv);
-	(void)envp;
+	//error_check(argc, argv);
+	(void) argc;
+	(void) argv;
 	while (1)
 	{
+		if (!config_tools(&tools, envp))
+		{
+			printf ("Error: Failed to allocate memory for tools->env\n");
+			free_tools(&tools);
+			exit (EXIT_FAILURE);
+		}
 		line = readline("\033[1;32mminishell\033[0m \033[1;34m➜\033[0m  ");
 		add_history_file(line);
-		free(line);   
+		free_tools(&tools);
+		free(line);
 	}
 	return (0);
 }
