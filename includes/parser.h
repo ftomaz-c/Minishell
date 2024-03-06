28ef8df0ffbef5e6dd54b621c09c8dd65423fe5a
ftomaz-c <fabiocastim@gmail.com>
2024-03-06 18:12:16 +0000

    main updated

--- includes/parser.h
+++ includes/parser.h
@@ -4,14 +4,14 @@
 # include "minishell.h"
 
 /*src/parser/parser.c*/
-int			parse_words(t_parser *node, t_lexer *current, int *i);
 t_lexer		*parse_tokens(t_parser *node, t_lexer *current, int *start);
+int			parse_words(t_parser *node, t_lexer *current);
 int			get_command(t_parser *node, t_lexer *lexer, int *start, int end);
 int			parse_lexer(t_parser **parser, t_lexer *lexer, int *start, int end);
 int			parser(t_tools *tools);
 
 /*src/parser/parser_redir.c*/
-t_lexer		*add_redirection(t_lexer *current, t_parser *node);
+t_lexer		*add_redirection(t_lexer *current, t_parser *node, int *start);
 
 /*src/parser/parser_utils.c*/
 void		free_strlist(char **str);
