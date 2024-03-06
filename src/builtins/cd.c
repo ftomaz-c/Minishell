2ed9d8e848587db3eb3d21120ebd370d5f0af58f
ftomaz-c <fabiocastim@gmail.com>
2024-03-06 11:25:51 +0000

    I panicd...

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -135,25 +135,19 @@ void	cd_err(int err, char *str, char root)
 		ft_putstr_fd("cd: ", STDERR_FILENO);
 	if (str)
 		ft_putstr_fd(str, STDERR_FILENO);
-	if (err == 25)
-	{
+	if (err == 1)
+		ft_putstr_fd("too many arguments\n", STDERR_FILENO);
+	else if (err == 2)
+		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+	else if (err == 13)
+		ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
+	else if (err == 20)
+		ft_putstr_fd(": Not a directory\n", STDERR_FILENO);
+	else if (err == 25)
 		ft_putstr_fd(": Is a directory\n", STDERR_FILENO);
-		g_status = 126;
-	}
-	else
-	{
-		if (err == 1)
-			ft_putstr_fd("too many arguments\n", STDERR_FILENO);
-		else if (err == 2)
-			ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-		else if (err == 3)
-			ft_putstr_fd(": Not a directory\n", STDERR_FILENO);
-		else if (err == 4)
-			ft_putstr_fd(": File name too long\n", STDERR_FILENO);
-		else if (err == 5)
-			ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
-		g_status = EXIT_FAILURE;
-	}
+	else if (err == 36)
+		ft_putstr_fd(": File name too long\n", STDERR_FILENO);
+	g_status = EXIT_FAILURE;
 }
 
 int	cd_handle_specific_path(t_tools *tools, t_parser *command)
@@ -168,16 +162,7 @@ int	cd_handle_specific_path(t_tools *tools, t_parser *command)
 		tools->pwd = ft_strdup(getcwd(new_pwd, sizeof(new_pwd)));
 	}
 	else if ((chdir(command->str[1]) < 0) && command->str[1][0])
-	{
-		if (errno == 2)
-			cd_err(2, command->str[1], 0);
-		if (errno == 20)
-			cd_err(3, command->str[1], 0);
-		if (errno == 36)
-			cd_err(4, command->str[1], 0);
-		if (errno == 13)
-			cd_err(5, command->str[1], 0);
-	}
+		cd_err(errno, command->str[1], 0);
 	return (g_status);
 }
 
@@ -230,7 +215,7 @@ int	cd_path(t_tools *tools, t_parser *command)
 			tools->pwd = ft_strdup(getcwd(new_pwd, sizeof(new_pwd)));
 		}
 		else
-			cd_err(2, command->str[1], 0);
+			cd_err(errno, command->str[1], 0);
 	}
 	else if (ft_strcmp(command->str[1], ".") == 0)
 		return (EXIT_SUCCESS);
@@ -274,6 +259,10 @@ int	cd(t_tools *tools, t_parser *command)
 	{
 		chdir(command->str[0]);
 		cd_err(errno, command->str[0], '/');
+		if (errno == 2)
+			g_status = 127;
+		else if (errno == 25)
+			g_status = 126;
 	}
 	else if ((command && !command->str[1]) || ft_strcmp(command->str[1], "~") == 0)
 		cd_no_path(tools, command);
