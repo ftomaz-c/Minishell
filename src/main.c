90b4ec9d369133c2f09b4584918979fc280ab97c
CarolinaRVB <carolinavbastos@hotmail.com>
2024-05-09 13:55:56 +0100

    handling isatty

--- src/main.c
+++ src/main.c
@@ -13,7 +13,23 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		handle_sigaction(react_sig_handler);
-		line = readline(".minishell: ");
+		if (!isatty(STDIN_FILENO))
+		{
+			ft_putstr_fd(".minishell: ", STDOUT_FILENO);
+			line = get_next_line(1);
+			if (line)
+			{
+				char *newline = ft_calloc(sizeof(char *), ft_strlen_nl(line));
+				ft_memcpy(newline, line, ft_strlen_nl(line));
+				if (newline)
+				{
+					free(line);
+					line = newline;
+				}
+			}
+		}
+		else
+			line = readline(".minishell: ");
 		if (!line)
 		{
 			ft_putstr_fd("exit\n", STDIN_FILENO);
--- src/minishell/signals.c
+++ src/minishell/signals.c
@@ -9,12 +9,21 @@ void	ignore_sig_handler(int sig)
 
 void	react_sig_handler(int sig)
 {
+	
 	if (sig == SIGINT)
 	{
-		printf("\n");
-		rl_replace_line("", 1);
-		rl_on_new_line();
-		rl_redisplay();
+		if (!isatty(STDIN_FILENO))
+		{
+			printf("\n");
+			ft_putstr_fd(".minishell: ", STDOUT_FILENO);
+		}
+		else
+		{
+			printf("\n");
+			rl_replace_line("", 1);
+			rl_on_new_line();
+			rl_redisplay();
+		}
 	}
 }
 
